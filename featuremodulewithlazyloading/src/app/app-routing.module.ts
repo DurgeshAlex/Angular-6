f40fbbdae885857af5331491ca9c4e6697d289a8
TechnicalGuru <durgeshkumarcse0051@gmail.com>
2019-03-01 22:00:16 +0530

    Removing multiple time load of app.module.html

--- featuremodulewithlazyloading/src/app/app-routing.module.ts
+++ featuremodulewithlazyloading/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path:'' ,redirectTo:'/app', pathMatch:'full'},
-  {path:'app', component:AppComponent},
   {path:'app/reservation', loadChildren:'./reservation/reservation.module#ReservationModule'},
   {path:'app/passenger', loadChildren:'./passenger/passenger.module#PassengerModule'}
 
